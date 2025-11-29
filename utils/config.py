9a667579148ef3344cfaf02ed0600281302c688e
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-29 12:34:35 +0530

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+S3_BUCKET = os.getenv("S3_BUCKET")
+
+DEEPSEEK_API_URL = "https://api.deepseek.com/v1/chat/completions"
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
+TENDERS_COLLECTION = os.getenv("TENDERS_COLLECTION")
+DOCS_STATUS_COLLECTION = os.getenv("DOCS_STATUS_COLLECTION")
+
+MAX_PROCESSES_GROQ = 5
+MAX_PROCESSES_DEEPSEEK = 10
+
