a13f662de85bafc0e78a1376d94b17bbb65a3dc6
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-29 11:48:13 +0530

    Create mongo_utils.py

---
+++ utils/mongo_utils.py
@@ -0,0 +1,33 @@
+from bson.objectid import ObjectId
+from pymongo import MongoClient, ReturnDocument
+from utils.config import MONGO_URI, DB_NAME, TENDERS_COLLECTION, DOCS_STATUS_COLLECTION
+
+mongo = MongoClient(MONGO_URI)
+db = mongo[DB_NAME]
+
+tenders_collection = db[TENDERS_COLLECTION]
+docs_status_collection = db[DOCS_STATUS_COLLECTION]
+ALLOWED_INDUSTRIES = ["Water & Sanitation", "Power & Energy"]
+
+def get_tender_ids(min_value):
+    cursor = tenders_collection.find(
+        {
+            "tender_value": {"$gte": min_value},
+            "industries": {"$in": ALLOWED_INDUSTRIES}  # filter by industries
+        },
+        {"_id": 1}
+    )
+    return [str(doc["_id"]) for doc in cursor]
+
+def is_document_complete(tender_id, document_name):
+    record = docs_status_collection.find_one(
+        {"tender_id": tender_id, "completed_forms": document_name}
+    )
+    return record is not None
+
+def mark_document_complete(tender_id, document_name):
+    docs_status_collection.update_one(
+        {"tender_id": tender_id},
+        {"$addToSet": {"completed_forms": document_name}},
+        upsert=True
+    )
